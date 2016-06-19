2aa0a6c07faf4281ab98b49c0669a706f5a6e415
KAORI KAO <youkai1472@icloud.com>
2016-06-19 16:43:47 +0900

    Initial Commit

---
+++ FashionCalendar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FashionCalendar
+//
+//  Created by erijae on 2016/06/19.
+//  Copyright © 2016年 erijae. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
